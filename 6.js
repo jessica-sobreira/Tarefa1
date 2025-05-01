babba836771f8d952cbd611ea8f1bccb3acc117c
Jessica <j.jecristina@gmail.com>
2025-05-01 15:22:54 -0300

    atividade 6

--- 6.js
+++ 6.js
@@ -1,4 +1,4 @@
-// 6. Ler três valores para os lados de um triângulo: A, B e C. Verificar se os lados fornecidos formam realmente um triângulo. Caso forme, deve ser indicado o tipo de triângulo: Isósceles, escaleno ou eqüilátero.
+// 6. Ler três valores para os lados de um triângulo: A, B e C. Verificar se os lados fornecidos formam realmente um triângulo. Caso forme, deve ser indicado o tipo de triângulo: Isósceles, escaleno ou eqüilátero. Para verificar se os lados fornecidos formam triângulo: A < B + C e B < A + C e C < A + B Triângulo isósceles: possui dois lados iguais (A=B ou A=C ou B = C) Triângulo escaleno: possui todos os lados diferentes (A<>B e B <> C) Triângulo eqüilátero: possui todos os lados iguais (A=B e B=C)
 
 const prompt = require("prompt-sync")()
 
@@ -6,10 +6,14 @@ let ladoA = Number(prompt("Informe o lado A: "))
 let ladoB = Number(prompt("Informe o lado B: "))
 let ladoC = Number(prompt("Informe o lado C: "))
 
-if(ladoA == ladoB && ladoB == ladoC){
-    console.log("Equilatero")
-}else if(ladoA == ladoB || ladoB == ladoC || ladoA == ladoC){
-    console.log("Isosceles")
+if(ladoA < ladoB + ladoC && ladoB < ladoA + ladoC && ladoC < ladoA + ladoB){
+    if(ladoA == ladoB && ladoA == ladoC){
+        console.log("Triângulo Equilatero")
+    }else if(ladoA == ladoB || ladoA == ladoC || ladoB == ladoC){
+        console.log("Triângulo Isóceles")
+    }else{
+        console.log("Triângulo Escaleno")
+    }
 }else{
-    console.log("Escaleno")
+    console.log("Os lados informados não formam um triângulo")
 }
