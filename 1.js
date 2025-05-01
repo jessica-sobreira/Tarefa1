d6d99c029d7c0b023f44fae7d370b305580c62a9
Jessica <j.jecristina@gmail.com>
2025-04-30 23:00:45 -0300

    começando a atividade

--- 1.js
+++ 1.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+//1. Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar utilizando uma estrutura de controle if.
+
+const prompt = require("prompt-sync")()
+
+let num = prompt("Informe um número: ")
+
+if(num % 2 == 0){
+    console.log(`O número ${num} é par`)
+}else{
+    console.log(`O número ${num} é ímpar`)
+}
+
