49990cfec41185d6975131a66a651fd8b49f82d4
Jessica <j.jecristina@gmail.com>
2025-05-01 15:45:53 -0300

    atividade 9

---
+++ 9.js
@@ -0,0 +1,8 @@
+// 9. Implemente um programa que exibe uma contagem regressiva de 10 até 1 no console utilizando um loop for.
+
+
+let num = 10
+
+for(i = 0; i < num; i++) {
+    console.log(num - i)
+}
